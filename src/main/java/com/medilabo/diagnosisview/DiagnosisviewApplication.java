f8d5b27fb2d94f6024888944050ce49dfa2abedb
TotoLanson <jbmassee@gmail.com>
2024-07-02 11:43:23 +0200

    Commit initial : Diagnosis View

---
+++ src/main/java/com/medilabo/diagnosisview/DiagnosisviewApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.diagnosisview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnosisviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagnosisviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medilabo/diagnosisview/DiagnosisviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.diagnosisview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagnosisviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
